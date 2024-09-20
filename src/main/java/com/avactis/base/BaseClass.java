169c95af0988a8ef9e0b499767dc8d2857d77b6e
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2024-09-20 22:32:03 +0530

    Modified BaseClass

--- src/main/java/com/avactis/base/BaseClass.java
+++ src/main/java/com/avactis/base/BaseClass.java
@@ -3,7 +3,9 @@ package com.avactis.base;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,77 +14,75 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
-import com.avactis.pageobjects.LoginPage;
 import com.avactis.utils.WaitUtils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 
-	public static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-	//public static WebDriver driver;
-	public static Properties prop;
-	public static WaitUtils waitUtils;
-	
-	
-	// Constructor: Loads config and initializes WaitUtils
+    public static WebDriver driver;
+    public static Properties prop;
+    public static WaitUtils waitUtils;
+    
+    // Constructor: Loads config and initializes WaitUtils
     public BaseClass() {
         loadConfig(); // Load properties from config file
-        if (getDriver() != null) {
-            waitUtils = new WaitUtils(getDriver()); 
+        if (driver != null) {
+            waitUtils = new WaitUtils(driver); // Initialize WaitUtils only if driver is not null
         }
- // Initialize WaitUtils only if driver is not null
-        }
-	
-	public void loadConfig()
-	{
-		try {
-			prop = new Properties();
-			System.out.println("Super constructor invoked");
-			FileInputStream ip = new FileInputStream(
-					System.getProperty("user.dir") + "\\Configuration\\config.properties");
-			prop.load(ip);
-			System.out.println("driver:" +driver);
-			
-		} catch(FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	// Launch browser based on the provided browser name
-	
-	public void launchApp(String browserName) {
-		// String browserName = prop.getProperty("browser");
-		if (browserName.equalsIgnoreCase("Chrome")) {
-			WebDriverManager.chromedriver().setup();
-			// Set Browser to ThreadLocalMap
-			driver.set(new ChromeDriver());
-		} else if (browserName.equalsIgnoreCase("FireFox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver.set(new FirefoxDriver());
-		} else if (browserName.equalsIgnoreCase("IE")) {
-			WebDriverManager.iedriver().setup();
-			driver.set(new InternetExplorerDriver());
-		}
-		 waitUtils = new WaitUtils(getDriver()); // Reinitialize WaitUtils after setting the driver
-}
-	
-	// Method to get WebDriver instance from ThreadLocal
-    public static WebDriver getDriver() {
-        return driver.get();
     }
-	
-	 // Click method with explicit wait
+    
+    // Method to load the configuration file
+    public void loadConfig() {
+        try {
+            prop = new Properties();
+            System.out.println("Super constructor invoked");
+            FileInputStream ip = new FileInputStream(
+                    System.getProperty("user.dir") + "\\Configuration\\config.properties");
+            prop.load(ip);
+            System.out.println("driver: " + driver);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    // Method to launch browser and application
+    public void launchApp(String browserName) {
+        if (browserName.equalsIgnoreCase("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("FireFox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("IE")) {
+            WebDriverManager.iedriver().setup();
+            driver = new InternetExplorerDriver();
+        }
+
+        // Maximize window and delete cookies
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        
+        // Launch the URL from the config file
+        driver.get(prop.getProperty("url"));
+        
+        waitUtils = new WaitUtils(driver); // Reinitialize WaitUtils after setting the driver
+    }
+    
+    // Click method with explicit wait
     public void click(By locator) {
-		WebElement element = waitUtils.waitForElementToBeVisible(locator);
+        WebElement element = waitUtils.waitForElementToBeVisible(locator);
         element.click();
     }
 
     // SendKeys method with explicit wait
     public void sendKeys(By locator, String text) {
-		WebElement element = waitUtils.waitForElementToBeVisible(locator);
+        WebElement element = waitUtils.waitForElementToBeVisible(locator);
         element.clear();
         element.sendKeys(text);
     }
@@ -102,9 +102,10 @@ public class BaseClass {
             return false;
         }
     }
-     // Method to quit WebDriver instance
-        public static void quitDriver() {
-            getDriver().quit();
-            driver.remove(); // Remove the WebDriver instance to avoid memory leak
+
+    // Method to quit WebDriver instance
+    public static void quitDriver() {
+        driver.quit();
+        driver = null; // Set driver to null to avoid memory leak
     }
 }
