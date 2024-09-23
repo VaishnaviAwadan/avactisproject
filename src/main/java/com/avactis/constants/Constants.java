e3ad0fc8f30b5d1f0f6ded5050587a239e97ad77
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2024-09-24 00:17:41 +0530

    Added Constants class to store reusable values

--- src/main/java/com/avactis/constants/Constants.java
+++ src/main/java/com/avactis/constants/Constants.java
@@ -4,6 +4,35 @@ import com.avactis.utility.ReadConfig;
 
 public class Constants {
 	
-	public static final String URL = ReadConfig.getproperty("url");
+	// File Paths
+    public static final String CONFIG_FILE_PATH = "./Configuration/config.properties";
+    public static final String DRIVER_PATH = "./Drivers/";
+    
+    // URLs
+    public static final String url = "http://localhost/Avactis";
+    
+    //Admin URLs
+    public static final String ADMIN_URL = "http://localhost/avactis/avactis-system/admin/signin.php";
+    public static final String ADMIN_USER_NAME = "vaishnaviawadan2002gmail.com";
+    public static final String ADMIN_PASSWORD = "@Vaishnavi#93";
+    
+    // Browser Types
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String EDGE = "edge";
+    
+    // Timeouts
+    public static final int IMPLICIT_WAIT = 10;
+    public static final int EXPLICIT_WAIT = 20;
+    
+    // Test Data
+    public static final String USERNAME = "testuser";
+    public static final String PASSWORD = "testpass";
+    
+    // Extent Reports
+    public static final String REPORTS_PATH = "./Reports/ExtentReport.html";
+    
+    // Others (you can add more as per your needs)
+    public static final String SCREENSHOTS_PATH = "./Screenshots/";
 
 }
