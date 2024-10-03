4ce012b314a2e102ffb542eaaf4550c306f1e7e2
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2024-10-04 01:22:35 +0530

    AddessPage added

---
+++ src/main/java/com/avactis/pageobjects/AddressPage.java
@@ -0,0 +1,26 @@
+package com.avactis.pageobjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.avactis.actiondriver.Action;
+import com.avactis.base.BaseClass;
+
+public class AddressPage extends BaseClass {
+
+	Action action = new Action();
+	
+	@FindBy(xpath ="//a[text()='Checkout']")
+	private WebElement proceedToCheckOut;
+	
+	public AddressPage() {
+		PageFactory.initElements(getDriver(), this);
+		
+	}
+	
+	public ShippingPage clickOnCheckOut() throws Throwable {
+		action.click(getDriver(), proceedToCheckOut );
+		return new shippingPage(getDriver());
+	}
+}
