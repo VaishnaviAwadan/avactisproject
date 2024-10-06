a2113f4a4d31007c6406ed8d31b61f3a013a6f6f
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2024-10-06 16:40:14 +0530

    'RegistrationTest' and 'IndexTest' added

--- src/main/java/com/avactis/pageobjects/IndexPage.java
+++ src/main/java/com/avactis/pageobjects/IndexPage.java
@@ -1,6 +1,61 @@
 package com.avactis.pageobjects;
 
-public class IndexPage {
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.avactis.actiondriver.Action;
+import com.avactis.base.BaseClass;
+
+public class IndexPage extends BaseClass {
+	
+	Action action = new Action();
+	
+	@FindBy(xpath = "//a[text()='Sign In']") 
+	private WebElement signInBtn;
+	
+	@FindBy(xpath = "//a[@class='site-logo']")
+	private WebElement avactisLogo;
+	
+	@FindBy(xpath = "//input[@type='text' and @name='search_pattern' and @class='form-control input_text']")
+	private WebElement searchProductBox;
+	
+	@FindBy(xpath = "//button[@type='submit' and text()='Search']")
+	private WebElement searchButton;
+	
+	// Modified constructor to accept WebDriver as a parameter
+	public IndexPage(WebDriver driver) {
+		PageFactory.initElements(driver, this); // Use the passed driver instance
+	}
+	
+	public LoginPage clickOnSignIn() throws Throwable {
+		action.fluentWait(getDriver(), signInBtn, 10);
+		action.click(getDriver(), signInBtn);
+		return new LoginPage(getDriver());
+	}
+	
+	public boolean validateLogo() throws Throwable {
+		// Use explicit wait
+		WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOf(avactisLogo)); // Wait until logo is visible
+		return avactisLogo.isDisplayed();
+	}
+	
+	public String getAvactisTitle() {
+		String avactisTitle = getDriver().getTitle();
+		return avactisTitle;
+	}
+	
+	public SearchResultPage searchProduct(String productName) throws Throwable {
+		action.type(searchProductBox, productName);
+		action.scrollByVisibilityOfElement(getDriver(), searchButton);
+		action.click(getDriver(), searchButton);
+		Thread.sleep(3000);
+		return new SearchResultPage();
+	}
 }
-  
---
+++ src/test/java/com/avactis/testcases/IndexTest.java
@@ -0,0 +1,40 @@
+package com.avactis.testcases;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.Assert;  // Corrected import
+import com.avactis.base.BaseClass;
+import com.avactis.pageobjects.IndexPage;
+
+public class IndexTest extends BaseClass {
+
+    private IndexPage indexPage;
+    
+    @BeforeMethod
+    public void setup() {
+        // Launch the application with the desired browser
+        launchApp(prop.getProperty("browser")); // Fetch browser from config file
+
+        // Initialize IndexPage after launching the app
+        indexPage = new IndexPage(getDriver()); // IndexPage should be initialized here
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // Quit the WebDriver after the test
+    	quitDriver();
+    }
+
+    @Test
+    public void verifyLogo() throws Throwable {
+        boolean result = indexPage.validateLogo();
+        Assert.assertTrue(result); // Added failure message
+    }
+
+    @Test
+    public void verifyTitle() {
+        String actTitle = indexPage.getAvactisTitle();
+        Assert.assertEquals(actTitle, "Avactis Demo Store"); // Fixed the expected title to match the actual one
+    }
+}
