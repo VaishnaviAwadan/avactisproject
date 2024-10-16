d6df2442305d9c3b071d152581eddef977a83229
Vaishnavi Awadan <vaishnavi88awadan@gmail.com>
2024-10-16 23:39:11 +0530

    modified LoginPageTest

--- src/test/java/com/avactis/testcases/LoginPageTest.java
+++ src/test/java/com/avactis/testcases/LoginPageTest.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.avactis.base.BaseClass;
 import com.avactis.pageobjects.HomePage;
 import com.avactis.pageobjects.IndexPage;
@@ -18,14 +17,14 @@ public class LoginPageTest extends BaseClass {
 
     @BeforeMethod
     public void setup() {
-		// Launch the application using the browser specified in the config file
-        launchApp(); // No need for String browser argument; it's handled by config
+        // Launch the application using the browser specified in the config file
+        launchApp(); 
     }
 
     @AfterMethod
     public void tearDown() {
         // Quit the WebDriver after the test
-        driver.quit(); // Ensures proper driver shutdown
+        driver.quit(); 
     }
 
     @Test
@@ -36,19 +35,25 @@ public class LoginPageTest extends BaseClass {
         // Click on the Sign In button to navigate to the LoginPage
         loginPage = indexPage.clickOnSignIn();
         
-        // Wait for login page to load (optional sleep; use waitUtils for better handling)
+        // Wait for the login page to load
         Thread.sleep(3000);
+        
+        // Print the credentials being used for debugging
+        System.out.println("Username: " + prop.getProperty("userName"));
+        System.out.println("Password: " + prop.getProperty("password"));
         
         // Perform login with credentials from the config file
         homePage = loginPage.login(prop.getProperty("userName"), prop.getProperty("password"));
         
-        // Wait for home page to load (optional sleep; use proper waits in real tests)
+        // Wait for home page to load
         Thread.sleep(3000);
         
         // Validate that the current URL is the expected URL after login
         String actualUrl = homePage.getCurrURL();
-        Thread.sleep(3000);
+        System.out.println("Actual URL: " + actualUrl);
         String expectedUrl = "http://localhost/Avactis/index.php";
-        Assert.assertEquals(actualUrl,expectedUrl, "Login failed or URL mismatch after login.");
+        
+        // Ensure that the URL contains the expected part
+        Assert.assertTrue(actualUrl.contains(expectedUrl), "Login failed or URL mismatch after login.");
     }
 }
